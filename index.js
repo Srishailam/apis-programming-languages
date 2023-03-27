6834f7b6be01416738b2ad9980b1f06b1e012af7
Srishailam <sriyd531@gmail.com>
2023-03-26 21:49:02 -0400

    cors enabled

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
 const programmingLanguagesRouter = require("./routes/programmingLanguages");
 
+app.use(cors());
 app.use(express.json())
 app.use(
   express.urlencoded({
